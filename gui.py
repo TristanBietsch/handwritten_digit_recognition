42bc93acb70c6cbc3b8b76f874cbf2b6b32b5ea2
Tristan Bietsch <tristanmbietsch@gmail.com>
2021-06-30 15:39:32 -0500

    Start of GUI

---
+++ gui.py
@@ -0,0 +1,25 @@
+# Interactive GUI window to draw digits on canvas
+
+from keras.models import load_model
+from tkinter import *
+import tkinter as tk
+import win32gui
+from PIL import ImageGrab, Image
+import numpy as np
+
+model = load_model('mnist.h5')
+
+def predict_digit(img):
+    #resizing image to 28 x 28 pixels
+    img = img.resize((28, 28))
+    # convert rgb to grayscale
+    img = img.convert('L')
+    img = np.array(img)
+    # reshaping to support model input and normalizing
+    img = img.reshape(1,28,28,1)
+    img = img/255.0
+    #predicting the class
+    res = model.predict([img])[0]
+    return np.argmax(res), max(res)
+
+
